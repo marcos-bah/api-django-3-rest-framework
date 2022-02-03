22d24445cb63098303b9f9d460b4bc5b48992289
marcos-bah <marcos.barbosa@unfei.edu.br>
2022-02-03 13:01:22 -0300

    add rotas de aluno, curso, matricula e lista matriculas por aluno

---
+++ escola/views.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets, generics
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer
+
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+    
