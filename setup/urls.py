c9c0945f6c6eba1f2730b63b4e277d2c9035d66d
marcos-bah <marcos.barbosa@unfei.edu.br>
2022-02-06 17:00:32 -0300

    add authentication + rotas e finalizando curso

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunoViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculasAluno
+from escola.views import AlunoViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -12,4 +12,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('alunos/<int:pk>/matriculas/', ListaMatriculasAluno.as_view(), name='lista_matriculas_aluno'),
+    path('cursos/<int:pk>/alunos/', ListaAlunosMatriculados.as_view(), name='lista_alunos_matriculados'),
 ]
